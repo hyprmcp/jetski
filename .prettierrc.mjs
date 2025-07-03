4bcb7317b2a327fde89d6fc87af0f0e47c8689d4
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-07-03 14:45:21 +0200

    chore: add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'es5',
+};
+
+export default config;
