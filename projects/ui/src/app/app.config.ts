621ade659c02d80c203a2949b0aa1a138aadac0c
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-07-03 16:24:24 +0200

    chore: add oauth initial draft

--- projects/ui/src/app/app.config.ts
+++ projects/ui/src/app/app.config.ts
@@ -1,16 +1,38 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { OAuthService, provideOAuthClient } from 'angular-oauth2-oidc';
 import { routes } from './app.routes';
+
+async function initializeOAuth() {
+  const oauthService = inject(OAuthService);
+  console.log('configure');
+  oauthService.configure({
+    issuer: 'http://localhost:5556/dex',
+    redirectUri: location.origin + '/home',
+    clientId: 'ui',
+    scope: 'openid profile email',
+    responseType: 'code',
+    showDebugInformation: true,
+  });
+
+  console.log('try login');
+  return await oauthService.loadDiscoveryDocumentAndLogin();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
+    provideOAuthClient(),
+    provideAppInitializer(initializeOAuth),
   ],
 };
--- projects/ui/src/app/app.routes.ts
+++ projects/ui/src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Routes } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+const authGuard: CanActivateFn = () => {
+  const oauth = inject(OAuthService);
+  if (oauth.hasValidIdToken()) {
+    console.log('auth guard true');
+    return true;
+  } else {
+    console.log('auth guard false');
+    return false;
+  }
+};
+
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      /* Routes that require authentication go here */
+      {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent,
+      },
+    ],
+  },
+  /* Routes that don't require authentication go here */
+];
