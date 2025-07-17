d0d42c2bc89f7e7f4f4651639aa8b86c38d1fa35
christophenne <christoph.enne@glasskube.eu>
2025-07-17 17:08:08 +0200

    chore: undo changing order
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- internal/db/projects.go
+++ internal/db/projects.go
@@ -8,24 +8,6 @@ import (
 	"github.com/jetski-sh/jetski/internal/types"
 )
 
-func CreateProject(ctx context.Context, orgID, createdBy uuid.UUID, name string) (*types.Project, error) {
-	db := internalctx.GetDb(ctx)
-	rows, err := db.Query(ctx, `
-		INSERT INTO Project (created_by, organization_id, name)
-		VALUES (@createdBy, @orgID, @name)
-		RETURNING id, created_at, created_by, organization_id, name, latest_deployment_revision_id, latest_deployment_revision_event_id
-	`, pgx.NamedArgs{"orgID": orgID, "createdBy": createdBy, "name": name})
-	if err != nil {
-		return nil, err
-	}
-	result, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[types.Project])
-	if err != nil {
-		return nil, err
-	} else {
-		return result, nil
-	}
-}
-
 func GetProjectsForUser(ctx context.Context, userID uuid.UUID) ([]types.Project, error) {
 	db := internalctx.GetDb(ctx)
 	rows, err := db.Query(ctx, `
@@ -40,6 +22,24 @@ func GetProjectsForUser(ctx context.Context, userID uuid.UUID) ([]types.Project,
 		return nil, err
 	}
 	result, err := pgx.CollectRows(rows, pgx.RowToStructByName[types.Project])
+	if err != nil {
+		return nil, err
+	} else {
+		return result, nil
+	}
+}
+
+func CreateProject(ctx context.Context, orgID, createdBy uuid.UUID, name string) (*types.Project, error) {
+	db := internalctx.GetDb(ctx)
+	rows, err := db.Query(ctx, `
+		INSERT INTO Project (created_by, organization_id, name)
+		VALUES (@createdBy, @orgID, @name)
+		RETURNING id, created_at, created_by, organization_id, name, latest_deployment_revision_id, latest_deployment_revision_event_id
+	`, pgx.NamedArgs{"orgID": orgID, "createdBy": createdBy, "name": name})
+	if err != nil {
+		return nil, err
+	}
+	result, err := pgx.CollectExactlyOneRow(rows, pgx.RowToAddrOfStructByName[types.Project])
 	if err != nil {
 		return nil, err
 	} else {
