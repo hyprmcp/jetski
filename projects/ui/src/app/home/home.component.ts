6510b438aadfc30128a06fb4851313802c0b2593
christophenne <christoph.enne@glasskube.eu>
2025-07-17 08:42:50 +0200

    chore: delete unused home component and add further mise tasks
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- projects/ui/src/app/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { httpResource } from '@angular/common/http';
-
-@Component({
-  template: ` <h1>Hello, {{ user.value() }}</h1> `,
-})
-export class HomeComponent {
-  user = httpResource.text(() => `/api/v1/whoami`);
-}
