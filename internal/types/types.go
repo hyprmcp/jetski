d207ecf89a011f97abd738f17c8d3c645f1ab32d
christophenne <christoph.enne@glasskube.eu>
2025-07-25 08:48:08 +0200

    chore: move dashboard specific types
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- internal/types/types.go
+++ internal/types/types.go
@@ -99,22 +99,3 @@ type Context struct {
 	ContextPropertyID    uuid.UUID `db:"context_property_id" json:"contextPropertyId"`
 	ContextPropertyValue any       `db:"context_property_value" json:"contextPropertyValue"`
 }
-
-type ProjectSummary struct {
-	Project
-	Organization                  Organization             `json:"organization"`
-	LatestDeploymentRevision      *DeploymentRevision      `json:"latestDeploymentRevision,omitempty"`
-	LatestDeploymentRevisionEvent *DeploymentRevisionEvent `json:"latestDeploymentRevisionEvent,omitempty"`
-}
-
-type DeploymentRevisionSummary struct {
-	DeploymentRevision
-	Project                              Project                 `json:"project"`
-	Author                               UserAccount             `json:"author"`
-	ProjectLatestDeploymentRevisionEvent DeploymentRevisionEvent `json:"projectLatestDeploymentRevisionEvent"`
-}
-
-type Usage struct {
-	SessionCount int `db:"session_count" json:"sessionCount"`
-	RequestCount int `db:"request_count" json:"requestCount"`
-}
