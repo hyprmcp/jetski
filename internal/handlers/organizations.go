b8daeb924ff365b17e9581e29e29026df5ce07ad
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-07-30 16:32:26 +0200

    feat: add create organization handler
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/handlers/organizations.go
+++ internal/handlers/organizations.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,6 +11,7 @@ import (
 
 func OrganizationsRouter(r chi.Router) {
 	r.Get("/", getOrganizations)
+	r.Post("/", postOrganizationHandler())
 }
 
 func getOrganizations(w http.ResponseWriter, r *http.Request) {
@@ -23,4 +25,28 @@ func getOrganizations(w http.ResponseWriter, r *http.Request) {
 	}
 
 	RespondJSON(w, orgs)
+}
+
+func postOrganizationHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		user := internalctx.GetUser(ctx)
+
+		var orgReq struct {
+			Name string `json:"name"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&orgReq); err != nil {
+			Handle4XXError(w, http.StatusBadRequest)
+			return
+		}
+
+		if org, err := db.CreateOrganization(ctx, orgReq.Name); err != nil {
+			HandleInternalServerError(w, r, err, "create organization error")
+		} else if err := db.AddUserToOrganization(ctx, user.ID, org.ID); err != nil {
+			HandleInternalServerError(w, r, err, "create organization error")
+		} else {
+			RespondJSON(w, org)
+		}
+	}
 }
