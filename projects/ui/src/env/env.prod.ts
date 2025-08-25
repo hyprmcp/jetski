277e521be2232d1a9767c6dd93f6c2e92e1b404a
Jakob Steiner <kosmoz@users.noreply.github.com>
2025-08-25 14:38:53 +0200

    chore: change prod issuer uri to hyprmcp (#128)

--- projects/ui/src/env/env.prod.ts
+++ projects/ui/src/env/env.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from './types';
 export const environment: Environment = {
   production: true,
   oidc: {
-    issuer: 'https://auth.jetski.sh',
+    issuer: 'https://auth.hyprmcp.com',
     clientId: 'ui',
   },
 };
