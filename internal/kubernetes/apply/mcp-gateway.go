a9e05ad097c03e83a4c3e46fe711fcfc12439638
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-09-03 13:39:54 +0200

    chore: migrate gateway creation to applyconfig

--- internal/kubernetes/apply/mcp-gateway.go
+++ internal/kubernetes/apply/mcp-gateway.go
@@ -6,10 +6,9 @@ import (
 
 	"github.com/hyprmcp/jetski/internal/db"
 	"github.com/hyprmcp/jetski/internal/env"
-	"github.com/hyprmcp/jetski/internal/kubernetes/api/v1alpha1"
+	"github.com/hyprmcp/jetski/internal/kubernetes/applyconfiguration/api/v1alpha1"
 	"github.com/hyprmcp/jetski/internal/types"
 	"github.com/hyprmcp/jetski/internal/util"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -22,7 +21,7 @@ func MCPGateway(client client.Client) *mcpGatewayApplier {
 }
 
 func (a *mcpGatewayApplier) Apply(ctx context.Context, org types.Organization) error {
-	var gatewayProjects []v1alpha1.ProjectSpec
+	var gatewayProjects []*v1alpha1.ProjectSpecApplyConfiguration
 	if pss, err := db.GetProjectSummaries(ctx, org.ID); err != nil {
 		return err
 	} else {
@@ -30,42 +29,41 @@ func (a *mcpGatewayApplier) Apply(ctx context.Context, org types.Organization) e
 			if ps.LatestDeploymentRevisionID == nil {
 				continue
 			}
-			gatewayProjects = append(gatewayProjects, v1alpha1.ProjectSpec{
-				ProjectID:            ps.ID.String(),
-				ProjectName:          ps.Name,
-				DeploymentRevisionID: ps.LatestDeploymentRevision.ID.String(),
-				Authenticated:        ps.LatestDeploymentRevision.Authenticated,
-				Telemetry:            ps.LatestDeploymentRevision.Telemetry,
-				ProxyURL:             ps.LatestDeploymentRevision.ProxyURL,
-			})
+			spec := v1alpha1.ProjectSpec().
+				WithProjectID(ps.ID.String()).
+				WithProjectName(ps.Name).
+				WithDeploymentRevisionID(ps.LatestDeploymentRevision.ID.String()).
+				WithAuthenticated(ps.LatestDeploymentRevision.Authenticated).
+				WithTelemetry(ps.LatestDeploymentRevision.Telemetry)
+
+			if ps.LatestDeploymentRevision.ProxyURL != nil {
+				spec.WithProxyURL(*ps.LatestDeploymentRevision.ProxyURL)
+			}
+
+			gatewayProjects = append(gatewayProjects, spec)
 		}
 	}
 
-	err := a.client.Patch(
+	spec := v1alpha1.MCPGatewaySpec().
+		WithOrganizationID(org.ID.String()).
+		WithOrganizationName(org.Name).
+		WithAuthorization(
+			v1alpha1.AuthorizationSpec().
+				WithDynamicClientRegistration(
+					v1alpha1.DynamicClientRegistrationSpec().
+						WithPublicClient(org.Settings.Authorization.DCRPublicClient),
+				),
+		).
+		WithProjects(gatewayProjects...)
+
+	if org.Settings.CustomDomain != nil {
+		spec.WithCustomDomain(*org.Settings.CustomDomain)
+	}
+
+	err := a.client.Apply(
 		ctx,
-		&v1alpha1.MCPGateway{
-			TypeMeta: metav1.TypeMeta{
-				APIVersion: v1alpha1.GroupVersion.Identifier(),
-				Kind:       "MCPGateway",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      org.Name,
-				Namespace: env.GatewayNamespace(),
-			},
-			Spec: v1alpha1.MCPGatewaySpec{
-				OrganizationID:   org.ID.String(),
-				OrganizationName: org.Name,
-				CustomDomain:     org.Settings.CustomDomain,
-				Authorization: v1alpha1.AuthorizationSpec{
-					DynamicClientRegistration: v1alpha1.DynamicClientRegistrationSpec{
-						PublicClient: org.Settings.Authorization.DCRPublicClient,
-					},
-				},
-				Projects: gatewayProjects,
-			},
-		},
-		client.Apply,
-		&client.PatchOptions{Force: util.PtrTo(true), FieldManager: "jetski"},
+		v1alpha1.MCPGateway(org.Name, env.GatewayNamespace()).WithSpec(spec),
+		&client.ApplyOptions{Force: util.PtrTo(true), FieldManager: "jetski"},
 	)
 
 	if err != nil {
